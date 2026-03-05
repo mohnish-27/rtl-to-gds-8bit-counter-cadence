c61ee80c229cb6620f07c1b4ea961fb12fc6835f
mohnish-27 <167702122+mohnish-27@users.noreply.github.com>
2026-03-05 22:52:35 +0530

    Add files via upload

---
+++ rtl/counter.v
@@ -0,0 +1,13 @@
+module counter(count,rst,clk,m);
+input rst,clk,m;
+output reg [7:0]count;
+always@(posedge (clk) or negedge (rst))
+begin
+if(!rst)
+count=0;
+else if(m)
+count=count+1;
+else
+count=count-1;
+end
+endmodule			
